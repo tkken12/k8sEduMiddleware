2cfcb38eac7e98f25f2fa93606a8afb57f1c5f18
kimtaeyoung <tkken12@naver.com>
2022-12-21 18:42:36 +0900

    pod handler 부분에서 broker에게 request하는 모듈 통일

--- routes/v1/pod/get/getList.js
+++ routes/v1/pod/get/getList.js
@@ -1,15 +1,25 @@
-const { ReqeustHandler, GetMethodQueryParser } = require("../../../../common/util")
+const { RequestHandler, GetMethodQueryParser, SendResponse } = require("../../../../common/util")
 const { Constants } = require("../../../../common/const")
 const router = require("express").Router()
 
 router.get( "/", async(req, res) => {
 
-    ReqeustHandler({
+    let message
+    let code
+    
+    RequestHandler({
         "method" : Constants["REQUEST_PATH"]["METHOD"]["GET"],
         "reqPath": Constants["REQUEST_PATH"]["BROKER_PATH"]["API"]["V1"]["POD"]["GET"],
-        "query"  : { params: { command: "GET", type: "ALL", params: GetMethodQueryParser( { podName: "ab", podNamespaces: ["a","b"]}) } }
-    }).then( brokerRes => {  res.send(brokerRes["data"]) })
-    .catch(err => { console.log(err) })
+        "query"  : { params: { command: "GET", type: "ALL", params: GetMethodQueryParser( { podName: "", podNamespaces: []}) } }
+    }).then( brokerRes => {
+        message = brokerRes["data"]
+        code    = Constants["HTTP_STATUS_CODE"][200]
+    }).catch(err => { 
+        message = []
+        code = Constants["HTTP_STATUS_CODE"][401]
+    }).finally( () => {
+        SendResponse(res, { code: code, message: !!message["Message"] !== false ? message["Message"] : "" }) 
     })
+})
 
 module.exports = router
